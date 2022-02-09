ed9d10db243ada5162d50820f277270dc86013fa
kqeez69420x <99300566+kqeez@users.noreply.github.com>
2022-02-09 13:56:01 +0800

    Update and rename aosp_RMX2061.mk to lineage_RMX2061.mk

--- lineage_RMX2061.mk
+++ lineage_RMX2061.mk
@@ -12,10 +12,10 @@ TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS := false
 
 # Inherit some Project Radiant common stuff
-$(call inherit-product, vendor/aosp/config/commonmk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_RMX2061
+PRODUCT_NAME := lineage_RMX2061
 PRODUCT_DEVICE := RMX2061
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 6 Pro
